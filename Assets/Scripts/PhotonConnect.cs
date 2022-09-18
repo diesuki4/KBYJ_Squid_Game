372486c74247a0802c5fe6657fe5f138a65cdf2e
aroCoding <arocoding@gmail.com>
2022-09-18 16:58:51 +0900

    Photon LobbyManager 작성

--- Assets/Scripts/PhotonConnect.cs
+++ Assets/Scripts/PhotonConnect.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 
 public class PhotonConnect : MonoBehaviourPunCallbacks
 {
     void Start()
     {
         PhotonNetwork.ConnectUsingSettings();
+        
+        CreateRoom();
+
+        JoinRoom();
     }
 
     void Update() { }
@@ -26,5 +31,31 @@ public class PhotonConnect : MonoBehaviourPunCallbacks
         base.OnJoinedLobby();
 
         PhotonNetwork.LoadLevel("BridgeScene");
+    }
+    
+    
+    /* 방생성 */
+    private void CreateRoom()
+    {
+        // 1 방 옵션 설정
+        RoomOptions roomOptions = new RoomOptions();
+        
+        // 1-1 최대인원(0이면 최대인원 = 256)
+        // Return byte; => byte n = 10; => 1111 1111 = 256
+        roomOptions.MaxPlayers = 20;
+        
+        // 1-2 룸 리스트에 보이지 않게 || 보이게? 
+        roomOptions.IsVisible = true;   // 공개/비밀방 여부(default: true)
+        
+        // 2 방 생성 '요청'(방 옵션을 이용해서) 
+        PhotonNetwork.CreateRoom("SquidRoom", roomOptions);
+    }
+    
+    /* 방 참가 */
+    public void JoinRoom()
+    {
+        // 1 방 참가 '요청'
+        // PhotonNetwork.JoinRoom("XR_B반");
+        PhotonNetwork.JoinRoom("SquidRoom");
     }
 }
