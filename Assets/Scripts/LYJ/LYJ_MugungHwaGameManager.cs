c0cc165f0ca3d25d778b017644d121a9affb3576
aroCoding <arocoding@gmail.com>
2022-09-23 10:57:20 +0900

    Update LYJ_MugungHwaGameManager.cs

--- Assets/Scripts/LYJ/LYJ_MugungHwaGameManager.cs
+++ Assets/Scripts/LYJ/LYJ_MugungHwaGameManager.cs
@@ -6,12 +6,14 @@ using UnityEngine;
 public class LYJ_MugungHwaGameManager : MonoBehaviour
 {
     public Transform randomPos;
+    public GameObject yeoungHee;
     
     // Start is called before the first frame update
     void Start()
     {
         Transform trRandom = randomPos.GetChild(Random.Range(0, randomPos.childCount));
-        PhotonNetwork.Instantiate("Player", trRandom.position, trRandom.rotation);
+        GameObject player = PhotonNetwork.Instantiate("Player", trRandom.position, trRandom.rotation);
+        yeoungHee.GetComponent<LYJ_YeongHeeState>().player = player;
     }
 
     // Update is called once per frame
