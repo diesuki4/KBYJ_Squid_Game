ab7dff2b48f0bdb195b76336630309e9f2fdc398
Kyungbin Cho <diesuki4@gmail.com>
2022-09-28 16:33:48 +0900

    플레이어 생성 위치 버그 수정

--- Assets/Scripts/WaitingroomManager.cs
+++ Assets/Scripts/WaitingroomManager.cs
@@ -3,14 +3,33 @@ using System.Collections.Generic;
 using Photon.Pun;
 using UnityEngine;
 
-public class WaitingroomManager : MonoBehaviour
+public class WaitingroomManager : MonoBehaviourPun
 {
     public Transform trPos;
+    GameObject go;
+    int playerCount;
+    float uniqueValue;
     
     // Start is called before the first frame update
     void Start()
     {
         Transform randomTr = trPos.GetChild(PhotonNetwork.CurrentRoom.PlayerCount - 1);
-        PhotonNetwork.Instantiate("Player", randomTr.position, randomTr.rotation);
+        go = PhotonNetwork.Instantiate("Player", randomTr.position, randomTr.rotation);
+
+        uniqueValue = Random.Range(float.MinValue, float.MaxValue);
+        photonView.RPC("AddPlayerCount", RpcTarget.MasterClient, uniqueValue);
+    }
+
+    [PunRPC]
+    void AddPlayerCount(float unqValue)
+    {
+        photonView.RPC("SetPlayerPosition", RpcTarget.All, playerCount++, unqValue);
+    }
+
+    [PunRPC]
+    void SetPlayerPosition(int count, float unqValue)
+    {
+        if (Mathf.Approximately(uniqueValue, unqValue))
+            go.transform.position = trPos.GetChild(count).position;
     }
 }
