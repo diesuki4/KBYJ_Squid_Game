ed7ef6190b55ce7df98bd726030b7d57183f81ec
aroCoding <arocoding@gmail.com>
2022-10-03 12:13:04 +0900

    PhotonConnect.cs 변경

--- Assets/Scripts/PhotonConnect.cs
+++ Assets/Scripts/PhotonConnect.cs
@@ -82,6 +82,6 @@ public class PhotonConnect : MonoBehaviourPunCallbacks
     {
         base.OnJoinedRoom();
         
-        PhotonNetwork.LoadLevel("Mugunghwa");
+        PhotonNetwork.LoadLevel("LobbyScene");
     }
 }
