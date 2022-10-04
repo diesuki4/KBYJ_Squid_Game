92812c2a1c7d57f0ead2546c285f3b8b8b359fe8
Kyungbin Cho <diesuki4@gmail.com>
2022-10-04 13:11:11 +0900

    Update PhotonConnect.cs

--- Assets/Scripts/PhotonConnect.cs
+++ Assets/Scripts/PhotonConnect.cs
@@ -82,6 +82,6 @@ public class PhotonConnect : MonoBehaviourPunCallbacks
     {
         base.OnJoinedRoom();
         
-        PhotonNetwork.LoadLevel("WaitingroomScene");
+        PhotonNetwork.LoadLevel("LobbyScene");
     }
 }
